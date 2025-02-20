f54770b0ac1bf13a63d1ba9f259245b260fdcbf8
Valentin <valko27@abv.bg>
2025-02-21 01:21:57 +0200

    Add angular materials

--- client/src/app/app.config.ts
+++ client/src/app/app.config.ts
@@ -4,7 +4,8 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { appInterceptor } from './core/interceptors/app.interceptor';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideHttpClient(withInterceptors([appInterceptor]))],
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideHttpClient(withInterceptors([appInterceptor])), provideAnimationsAsync()],
 };
