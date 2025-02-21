46c9c22f9b1ba90872f9251954d49cc0abd8c1c3
Valentin <valko27@abv.bg>
2025-02-21 19:32:17 +0200

    Add profile page

--- client/src/app/features/user/pages/profile/profile.component.ts
+++ client/src/app/features/user/pages/profile/profile.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Signal } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { AuthService } from '../../../../core/services/auth.service';
+import { User } from '../../models/User';
 
 @Component({
   selector: 'app-profile',
@@ -7,6 +9,12 @@ import { RouterModule } from '@angular/router';
   templateUrl: './profile.component.html',
   styleUrl: './profile.component.css'
 })
-export class ProfileComponent {
+export class ProfileComponent implements OnInit{
+  user: User | null = null;
 
+  constructor(public authService: AuthService) { }
+
+  ngOnInit(): void {
+      this.user = this.authService.user();
+  }
 }
