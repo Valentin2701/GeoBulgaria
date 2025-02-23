a4ab094fdece0e11a111b2ec424c294b342dbe10
Valentin <valko27@abv.bg>
2025-02-23 20:41:13 +0200

    Fix server reload issue

--- server/middlewares/authMiddleware.js
+++ server/middlewares/authMiddleware.js
@@ -4,18 +4,17 @@ import { SECRET } from "../config.js";
 export const authMiddleware = async (req, res, next) => {
   const token = req.cookies["auth"];
 
-  if (!token) next();
+  if (!token) return next();
 
   try {
     const decodedToken = await jwt.verify(token, SECRET);
 
     req.user = decodedToken;
-
-    next();
   } catch (err) {
     res.clearCookie("auth");
     return res.status(401).json({ message: "Не сте регистриран" });
   }
+  next();
 };
 
 export const isAuth = (req, res, next) => {
