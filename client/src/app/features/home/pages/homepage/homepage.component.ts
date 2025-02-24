35b5e3309e43a673412d438987d8622a0da0b0fc
Valentin <valko27@abv.bg>
2025-02-24 15:19:03 +0200

    Add background images on homepage

--- client/src/app/features/home/pages/homepage/homepage.component.ts
+++ client/src/app/features/home/pages/homepage/homepage.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, signal, WritableSignal } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
 @Component({
@@ -7,6 +7,18 @@ import { RouterModule } from '@angular/router';
   templateUrl: './homepage.component.html',
   styleUrl: './homepage.component.css'
 })
-export class HomepageComponent {
+export class HomepageComponent implements OnInit{
 
+  images: string[] = ["шипка.jpg", "александър невски.jpg", "рилски езера.png", "белоградчик.jpg"];
+  index: number = 1;
+  imageUrl: WritableSignal<string> = signal("шипка.jpg");
+
+  constructor() { }
+
+  ngOnInit(): void {
+    setInterval(() => {
+      this.imageUrl.set(this.images[this.index]);
+      this.index = (this.index + 1) % this.images.length;
+    }, 2000);
+  }
 }
