92b1860958458788d5c2cfdb8dd2140fd3bbf081
Valentin <valko27@abv.bg>
2025-02-24 22:25:55 +0200

    Add test interface

---
+++ client/src/app/features/test/models/Test.ts
@@ -0,0 +1,10 @@
+import { Question } from "./Question";
+
+export interface Test{
+    _id: String,
+    title: String,
+    description?: String,
+    questions: Array<Question>,
+    createdAt: Date,
+    updatedAt: Date
+}
