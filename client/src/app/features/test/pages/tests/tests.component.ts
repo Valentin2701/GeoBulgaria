c57f177144a8b08595fcb04b5fa61d52bea3e22e
Valentin <valko27@abv.bg>
2025-02-25 11:20:55 +0200

    Get tests on tests page init

--- client/src/app/features/test/pages/tests/tests.component.ts
+++ client/src/app/features/test/pages/tests/tests.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Test } from '../../models/Test';
+import { TestService } from '../../services/test.service';
+import { LoaderService } from '../../../../core/services/loader.service';
+import { SnackbarService } from '../../../../core/services/snackbar.service';
 
 @Component({
   selector: 'app-tests',
@@ -6,6 +10,23 @@ import { Component } from '@angular/core';
   templateUrl: './tests.component.html',
   styleUrl: './tests.component.css'
 })
-export class TestsComponent {
+export class TestsComponent implements OnInit{
+  tests: Test[] = [];
 
+  constructor(private testService: TestService, private loaderService: LoaderService, private snackbarService: SnackbarService){}
+
+  ngOnInit(): void {
+    this.loaderService.showLoader();
+    
+    this.testService.getTests().subscribe(
+      (tests) => {
+        this.tests = tests;
+        this.loaderService.hideLoader();
+      },
+      (error) => {
+        this.loaderService.hideLoader();
+        this.snackbarService.showError("Couldn't fetch tests");
+      }
+    );
+  }
 }
