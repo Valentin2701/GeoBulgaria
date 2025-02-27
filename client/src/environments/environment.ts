c629f3038245c438324c65e647d61ee87193a205
Valentin <valko27@abv.bg>
2025-02-28 00:56:57 +0200

    FE ready for production ffs 😭

--- client/src/environments/environment.ts
+++ client/src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true,
-    API_URL: 'https://geobulgaria-1.onrender.com',
+    API_URL: process.env["API_URL"] || 'https://geobulgaria-1.onrender.com',
     appVersion: "1.0.0"
 };
